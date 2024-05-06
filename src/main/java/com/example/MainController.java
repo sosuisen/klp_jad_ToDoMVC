26e1ee77802f4410c70da5b6b8bc24725a87dac2
sosuisen <hidekazu.kubota@gmail.com>
2024-05-06 23:55:32 +0900

    fix: add menu event handler

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -6,16 +6,20 @@ import java.util.List;
 import com.example.model.ToDo;
 import com.example.model.ToDoManager;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -26,6 +30,12 @@ public class MainController {
 	private final String DATE_ID_POSTFIX = "-date";
 	private final String COMPLETED_ID_POSTFIX = "-completed";
 
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	private MenuItem menuItemClose;
+	
 	@FXML
 	private Button addBtn;
 
@@ -43,6 +53,14 @@ public class MainController {
 
 	private ToDoManager model;
 
+	private void showInfo(String txt) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("アプリの情報");
+		alert.setHeaderText(null);
+		alert.setContentText(txt);
+		alert.showAndWait();
+	}
+		
 	private HBox createToDoHBox(ToDo todo) {
 		// Create View Items
 		var completedCheckBox = new CheckBox();
@@ -143,8 +161,11 @@ public class MainController {
 	public void initialize() {
 		// Set today
 		headerDatePicker.setValue(LocalDate.now());
+		
+	    headerPriorityChoiceBox.getItems().addAll(1, 2, 3, 4, 5);
+	    headerPriorityChoiceBox.setValue(3);
 
-		headerPriorityChoiceBox.getItems().addAll(1, 2, 3, 4, 5);
-		headerPriorityChoiceBox.setValue(3);
+		menuItemAbout.setOnAction(e -> showInfo("ToDo App"));		
+		menuItemClose.setOnAction(e -> Platform.exit());
 	}
 }
