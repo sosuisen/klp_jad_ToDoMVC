b15acb9c67c8f2f5c92ec762efc9d788ebfa83a0
sosuisen <hidekazu.kubota@gmail.com>
2024-05-06 23:55:32 +0900

    fix: add menu event handler

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -6,13 +6,17 @@ import java.util.List;
 import com.example.model.ToDo;
 import com.example.model.ToDoManager;
 
+import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -20,6 +24,12 @@ import javafx.scene.layout.VBox;
 
 public class MainController {
 	private final String TODO_ID_PREFIX = "todo-";
+	
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	private MenuItem menuItemClose;
 	
 	@FXML
 	private Button addBtn;
@@ -35,6 +45,14 @@ public class MainController {
 
 	private ToDoManager model;
 
+	private void showInfo(String txt) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("アプリの情報");
+		alert.setHeaderText(null);
+		alert.setContentText(txt);
+		alert.showAndWait();
+	}
+		
 	private HBox createToDoHBox(ToDo todo) {
 		// Create View Items
 		var completedCheckBox = new CheckBox();
@@ -104,5 +122,8 @@ public class MainController {
 	public void initialize() {
 		// Set today
 		headerDatePicker.setValue(LocalDate.now());
+		
+		menuItemAbout.setOnAction(e -> showInfo("ToDo App"));		
+		menuItemClose.setOnAction(e -> Platform.exit());
 	}
 }
